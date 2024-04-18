f3188a882e15fa933b61b0a3f60013c2578101c5
Ivan <ijvv06242001@gmail.com>
2024-04-18 22:04:38 +0200

    Project init

---
+++ src/main/java/com/project/firstclicks/FirstclicksApplication.java
@@ -0,0 +1,13 @@
+package com.project.firstclicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstclicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstclicksApplication.class, args);
+	}
+
+}
