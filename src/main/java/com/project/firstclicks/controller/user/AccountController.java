67d84d7fbd4f2761a36839e1ac545fdce16e52f3
ferguttz <carlosfer856@gmail.com>
2024-05-07 10:23:33 +0200

    unified 2

--- src/main/java/com/project/firstclicks/controller/user/AccountController.java
+++
@@ -1,5 +0,0 @@
-package com.project.firstclicks.controller.user;
-
-public class AccountController {
-
-}
--- src/main/java/com/project/firstclicks/controller/user/UserWebMapController.java
+++
@@ -1,9 +0,0 @@
-package com.project.firstclicks.controller.user;
-
-//Aqui se almacenan las rutas para el api controller
-public class UserWebMapController {
-
-	public final static String USER_API_NEW_ADMIN = "private/api/user/admin";
-	public final static String USER_API_NEW_TUTOR = "private/api/user/register/tutor";
-	public final static String USER_API_NEW_STUDENT = "private/api/user/student";
-}
