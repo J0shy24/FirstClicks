6b13ec5ed06ab07e11dcfe02652659e6f8828595
Ivan <ijvv06242001@gmail.com>
2024-05-12 02:32:34 +0200

    corecciones

--- src/main/java/com/project/firstclicks/repository/CourseRepository.java
+++ src/main/java/com/project/firstclicks/repository/CourseRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.firstclicks.entity.Course;
 
-
-public interface CourseRepository extends JpaRepository<Course, Integer>{
+@Repository
+public interface CourseRepository extends JpaRepository<Course, CourseID>{
 	List<Course> findTop6ByOrderByCreatedDate();
 	
 	Optional<Course> findByName(String name);
--- src/main/java/com/project/firstclicks/repository/StudentCourseRepository.java
+++ src/main/java/com/project/firstclicks/repository/StudentCourseRepository.java
@@ -1,9 +1,11 @@
 package com.project.firstclicks.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.project.firstclicks.entity.StudentCourse;
-
-public interface StudentCourseRepository extends JpaRepository<StudentCourse,Integer>{
+import com.project.firstclicks.entityID.StudentCourseId;
+@Repository
+public interface StudentCourseRepository extends JpaRepository<StudentCourse,StudentCourseId>{
 
 }
